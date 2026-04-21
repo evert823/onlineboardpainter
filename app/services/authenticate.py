aa79708d7ee753225251c35c3eb567d267f2de57
Evert Jan Karman <89303498+evert823@users.noreply.github.com>
2026-04-21 16:13:51 +0200

    keywords from file

--- app/services/authenticate.py
+++ app/services/authenticate.py
@@ -3,9 +3,16 @@ from app.classes.fuzzyauthenticate import FuzzyAthenticate
 
 def verify_test_text(input: TextInput):
     # Your logic here
-    myfa = FuzzyAthenticate(keywords="norrell,strange")
+    myfa = FuzzyAthenticate(keywords=get_keywords_from_file())
     myresult = myfa.authenticate(inputtext=input.text)
     if myresult:
         return {"success": True, "message": "Authenticated!"}
     else:
         return {"success": False, "message": "Authentication failed."}
+
+def get_keywords_from_file():
+    filepath = "/home/administrator/onlineboardpainter/apikeywords.txt"
+    file1 = open(filepath, "r", encoding="utf-8")
+    keywords = file1.read().strip()
+    file1.close()
+    return keywords
